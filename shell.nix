4f89a59695e769e1894b300f2454cbe799c8743c
Zenna Allwein <zrallwein@gmail.com>
2024-01-24 23:25:51 -0600

    started roasting somethign new

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, lib, servant, servant-server
+      , wai, wai-cors, warp
+      }:
+      mkDerivation {
+        pname = "roasted";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          aeson base servant servant-server wai wai-cors warp
+        ];
+        license = lib.licenses.mit;
+        mainProgram = "roasted";
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
